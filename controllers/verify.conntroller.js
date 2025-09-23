06534f656f81ab03f5178959905ed2c7d2b52414
NaiveProgrammer() <guravmanas22@gmail.com>
2025-09-24 00:06:05 +0530

    Update verify.conntroller.js

--- controllers/verify.conntroller.js
+++ controllers/verify.conntroller.js
@@ -5,11 +5,11 @@ import supabaseService from "../services/supabase.service.js";
 
 dotenv.config();
 
-async function verify_tweet(tweet_content, tweet_context, tweet_language, embeddings) {
+async function verify_tweet(tweet_content, tweet_context, tweet_language, embeddings, tweet_date) {
 
   try {
 
-    const references = await supabaseService.fetchSimilarDocuments(embeddings) || [];
+    const references = await supabaseService.fetchSimilarDocuments(embeddings = embeddings, tweet_date = tweet_date) || [];
 
     const response = await LLMService.verifyContext(tweet_context, tweet_language, JSON.stringify(references));
     CacheService.setToContentCache(tweet_content, response);
