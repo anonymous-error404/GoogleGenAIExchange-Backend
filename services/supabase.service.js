018af54ec77367b424abbab2b3439f877e094e34
NaiveProgrammer() <guravmanas22@gmail.com>
2025-09-24 07:28:00 +0530

    Update supabase.service.js

--- services/supabase.service.js
+++ services/supabase.service.js
@@ -4,21 +4,25 @@ class SupabaseService {
 
     async fetchSimilarDocuments(embeddings, matchCount = 5, threshold = 0.3, tweet_date) {
 
+        try{
         const { data, error } = await supabaseClient.rpc('match_documents', {
             p_query_embedding: embeddings,
-            p_target_date: tweet_date,
+            //p_target_date: tweet_date,
             p_match_count: matchCount,
             p_similarity_threshold: threshold
         });
 
-        console.log('Supabase RPC result:', { data, error });
-
-        if (error) {
+            if (error) {
             console.error('Error fetching similar documents:', error);
-            throw error;
+            }
+
+            console.log('Supabase RPC result:', { data, error });   
+
+            return data;
         }
-        
-        return data;
+        catch(error){
+            return null;
+        }
     }
 }
 
